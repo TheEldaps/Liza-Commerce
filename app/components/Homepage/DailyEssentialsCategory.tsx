1a7023970f993291f641007d3b010ccaf149de6a
TheEldaps <adejames97@gmail.com>
2025-10-22 13:43:04 +0100

    Add DailyEssentialsItems

--- app/components/Homepage/DailyEssentialsCategory.tsx
+++ app/components/Homepage/DailyEssentialsCategory.tsx
@@ -1,5 +1,23 @@
-import React from "react";
+import Link from "next/link";
+import DailyEssentialsItems from "./DailyEssentialsItems";
 
 export default function DailyEssentialsCategory() {
-  return <section></section>;
+  return (
+    <section className="w-[90%] mx-auto font-[montserrat]">
+      <div className="flex justify-between mb-[20px]">
+        <p className="border-b-4 border-[#2418c0] pb-[5px]">
+          Daily <span className="text-[#2418c0] font-bold">Essentials</span>
+        </p>
+        <Link
+          href="/DailyEssentials"
+          className="text-[0.7rem] hover:text-[#000000ab] hover:underline my-auto "
+        >
+          View All <span className="text-[#2418c0] font-bold ml-[]">&gt;</span>{" "}
+        </Link>
+      </div>
+      <div className="flex justify-between flex-wrap ">
+        <DailyEssentialsItems />
+      </div>
+    </section>
+  );
 }
---
+++ app/components/Homepage/DailyEssentialsItems.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DailyEssentialsItems() {
+  return <div>DailyEssentialsItems</div>;
+}
