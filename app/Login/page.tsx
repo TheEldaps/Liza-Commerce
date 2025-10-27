30255c877ab39b96c6580a8491a6f009aac32ff7
TheEldaps <adejames97@gmail.com>
2025-10-27 12:52:19 +0100

    Add state to log ibn form

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -1,11 +1,16 @@
+"use client";
+
 import React from "react";
 import CartImage from "../../public/CartImage.png";
 import Image from "next/image";
 import Link from "next/link";
 import UsernameIcon from "../../public/UsernameIcon.png";
 import PasswordIcon from "../../public/PasswordIcon.png";
+import { useState } from "react";
 
 export default function Login() {
+  const [form, setForm] = useState("Login");
+
   return (
     <section className="flex items-center h-[100vh] bg-[#2418c0] bg-[url('/LoginBackgroundDecore.png')] bg-no-repeat bg-cover bg-center">
       <form
@@ -19,52 +24,144 @@ export default function Login() {
           alt="cart image"
           className="block w-[30%] max-w-[120px]  h-auto mb-[50px]"
         />
-        <div className="w-[50%] max-w-[300px]">
-          <div className="flex min-w-[60%] gap-3 mb-[15px] border border-white p-[7px] items-center rounded-sm hover:border-[#e3d2d2cd]">
-            <Image
-              src={UsernameIcon}
-              width="20"
-              height="20"
-              alt="Password Icon"
-              className="w-[15px] h-[15px] max-w-[50px]"
-            />
+
+        {form === "Login" && (
+          // FORM APPEARANCE FOR LOG IN
+          <div className="w-[50%] max-w-[300px]">
+            <div className="flex min-w-[60%] gap-3 mb-[15px] border border-white p-[7px] items-center rounded-sm hover:border-[#e3d2d2cd]">
+              <Image
+                src={UsernameIcon}
+                width="20"
+                height="20"
+                alt="Password Icon"
+                className="w-[15px] h-[15px] max-w-[50px]"
+              />
+              <input
+                type="text"
+                id="username"
+                className="w-[60%] h-auto outline-0 text-[0.7rem] text-[white] font-[montserrat] flex-1"
+                placeholder="USERNAME"
+              />
+            </div>
+
+            <div className="flex min-w-[60%] gap-3 border border-white p-[7px] items-center rounded-sm hover:border-[#e3d2d2cd] mb-[35px]">
+              <Image
+                src={PasswordIcon}
+                width="20"
+                height="20"
+                alt="Username Icon"
+              />
+              <input
+                type="password"
+                id="password"
+                className="w-[60%] h-auto outline-0 text-[0.7rem] text-[white] font-[montserrat] flex-1"
+                placeholder="PASSWORD"
+              />
+            </div>
+
+            <Link href="/">
+              <button className="bg-[white] text-[#2418c0] shadow-black shadow-sm block text-[0.8rem] w-[100%] py-[8px] font-[montserrat] font-medium rounded-sm mb-[5px] cursor-pointer hover:bg-[#2418c0] hover:text-[white]">
+                LOGIN
+              </button>
+            </Link>
+            <button
+              onClick={() => {
+                setForm("forgot password");
+              }}
+              className="text-right block text-[0.8rem] font-[montserrat] text-[white] hover:text-[#e3d2d2cd] "
+            >
+              Forgot password?
+            </button>
+
+            <div className="mt-[20px]">
+              <span className="text-white text-[0.8rem] font-[montserrat]">
+                Don&apos;t have an account?
+              </span>
+
+              <button
+                onClick={() => {
+                  setForm("create account");
+                }}
+                className="text-right sm:inline-block sm:ml-[10px] block text-[0.8rem] font-[montserrat] text-[white] hover:text-[#e3d2d2cd]"
+              >
+                Create Account
+              </button>
+            </div>
+          </div>
+        )}
+        {form === "create account" && (
+          // FORM APPEARANCE FOR CREATE ACCOUNT STATE
+          <div>
+            <div className="flex min-w-[60%] gap-3 mb-[15px] border border-white p-[7px] items-center rounded-sm hover:border-[#e3d2d2cd]">
+              <Image
+                src={UsernameIcon}
+                width="20"
+                height="20"
+                alt="Password Icon"
+                className="w-[15px] h-[15px] max-w-[50px]"
+              />
+              <input
+                type="text"
+                id="username"
+                className="w-[60%] h-auto outline-0 text-[0.7rem] text-[white] font-[montserrat] flex-1"
+                placeholder="USERNAME"
+              />
+            </div>
+            <div className="flex min-w-[60%] gap-3 border border-white p-[7px] items-center rounded-sm hover:border-[#e3d2d2cd] mb-[35px]">
+              <Image
+                src={PasswordIcon}
+                width="20"
+                height="20"
+                alt="Username Icon"
+              />
+              <input
+                type="password"
+                id="password"
+                className="w-[60%] h-auto outline-0 text-[0.7rem] text-[white] font-[montserrat] flex-1"
+                placeholder="PASSWORD"
+              />
+            </div>
+            <button className="bg-[white] text-[#2418c0] shadow-black shadow-sm block text-[0.8rem] w-[100%] py-[8px] font-[montserrat] font-medium rounded-sm mb-[5px] cursor-pointer hover:bg-[#2418c0] hover:text-[white]">
+              Create Account
+            </button>
+
+            <p className="font-[montserrat] text-[white] text-[0.8rem]  mt-[10px]">
+              Have an account?
+              <button
+                onClick={() => {
+                  setForm("Login");
+                }}
+                className="ml-[10px] hover:text-[#e3d2d2cd] cursor-pointer"
+              >
+                Log in
+              </button>
+            </p>
+          </div>
+        )}
+
+        {form === "forgot password" && (
+          <div>
             <input
               type="text"
               id="username"
               className="w-[60%] h-auto outline-0 text-[0.7rem] text-[white] font-[montserrat] flex-1"
               placeholder="USERNAME"
             />
+            <p>
+              Enter your registration email for a password reset link to be sent
+              to you
+            </p>
+            <Link
+              href="/"
+              onClick={() => {
+                setForm("Login");
+              }}
+              className="text-right hover:text-[#e3d2d2cd] cursor-pointer"
+            >
+              Log in
+            </Link>
           </div>
-
-          <div className="flex min-w-[60%] gap-3 border border-white p-[7px] items-center rounded-sm hover:border-[#e3d2d2cd] mb-[35px]">
-            <Image
-              src={PasswordIcon}
-              width="20"
-              height="20"
-              alt="Username Icon"
-            />
-            <input
-              type="password"
-              id="password"
-              className="w-[60%] h-auto outline-0 text-[0.7rem] text-[white] font-[montserrat] flex-1"
-              placeholder="PASSWORD"
-            />
-          </div>
-
-          <Link href="/">
-            <button className="bg-[white] text-[#2418c0] shadow-black shadow-sm block text-[0.8rem] w-[100%] py-[8px] font-[montserrat] font-medium rounded-sm mb-[5px] cursor-pointer hover:bg-[#2418c0] hover:text-[white]">
-              LOGIN
-            </button>
-          </Link>
-          <Link
-            href=""
-            className="text-right block text-[0.8rem] font-[montserrat] text-[white] hover:text-[#e3d2d2cd] "
-          >
-            Forgot password?
-          </Link>
-
-          <Link href="Create account">Create Account</Link>
-        </div>
+        )}
       </form>
     </section>
   );
