193ded9e0cf74694d6c4e2b8f952313ed8f3c2fd
TheEldaps <adejames97@gmail.com>
2025-10-29 23:54:43 +0100

    Add nav to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import Nav from "../app/components/Homepage/Nav";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -33,6 +34,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${montserrat.variable} antialiased`}
       >
+        <Nav />
+
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import Footer from "./components/Homepage/Footer";
 export default function Home() {
   return (
     <div className="min-h-[500vh]">
-      <Nav />
+      {/* <Nav /> */}
       <CategoryList />
       <BannerSection />
       <BestDealSection />
